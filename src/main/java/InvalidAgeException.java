06bcaaf804ee7c23fbcb43b3f999b348f0954c7d
swayampakulabhargava <78176565+swayampakulabhargava@users.noreply.github.com>
2021-04-06 23:24:05 -0500

    added

---
+++ src/main/java/InvalidAgeException.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bharath
+ */
+public class InvalidAgeException extends Exception{
+
+    public InvalidAgeException(String errorMessage) {
+        super(errorMessage);
+    }
+    
+}
+    
+
